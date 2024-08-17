5ffe874f1e2d4ec38b788ca84c8151d74c6c7758
Aachuyadv <aanchalydv31@gmail.com>
2024-08-17 22:29:21 +0530

    Create javax.java
    this is my project that i made while doing the forage software developer teaining

---
+++ javax.java
@@ -0,0 +1,93 @@
+package com.example.demo.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "financial_advisors")
+public class FinancialAdvisor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @Email(message = "Invalid email")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Phone is required")
+    private String phone;
+
+    public FinancialAdvisor() {}
+
+    public FinancialAdvisor(@NotNull String name, @NotNull String email, @NotNull String phone) {
+        if (name == null || email == null || phone == null) {
+            throw new NullPointerException("Input parameters cannot be null");
+        }
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "FinancialAdvisor{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FinancialAdvisor that = (FinancialAdvisor) o;
+        return id.equals(that.id) &&
+                name.equals(that.name) &&
+                email.equals(that.email) &&
+                phone.equals(that.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(id, name, email, phone);
+    }
+}
